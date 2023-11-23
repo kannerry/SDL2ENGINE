cc42840c5d5d402f65aa420d28770ea8085ef43a
kannerry <46495718+kannerry@users.noreply.github.com>
2023-11-23 21:43:49 +0000

    BASIC RENDER FUNCTION + small changes
    +render loop
    + debug/release add/remove console
    + reference stuff

--- main.cpp
+++ main.cpp
@@ -2,20 +2,29 @@
 #define SDL_MAIN_HANDLED
 #include "sdl_helper.h"
 
+void process_event(SDL_Event *event, WindowContainer *window_container) {
+	sdl_default_process_event(event, window_container);
+}
+
+void render(WindowContainer* window_container) {
+	sdl_default_render_clear(window_container, SDL_Color{ 0, 0, 0, SDL_ALPHA_OPAQUE }); // clear color
+	// ^
+	SDL_SetRenderDrawColor(window_container->renderer, 255, 0, 0, SDL_ALPHA_OPAQUE);
+	SDL_RenderDrawLine(window_container->renderer, 0, 0, 10, 10);
+	// V
+	sdl_default_render_present(window_container); // present
+}
+
 int main(int argc, char* args[]) {
 	WindowContainer window_container{};
-	sdl_init("Window Name", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 100, window_container);
+	sdl_init("Window Name", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 100, &window_container);
 
 	while (window_container.alive) {
 		SDL_Event event;
-		SDL_PollEvent(&event);
-		switch (event.type)
-		{
-			case SDL_QUIT:
-				sdl_cleanup(window_container);
-			default:
-				break;
+		while (SDL_PollEvent(&event)) {
+			process_event(&event, &window_container);
 		}
+		render(&window_container);
 	}
 
 	return 0;
--- sdl_helper.h
+++ sdl_helper.h
@@ -12,11 +12,11 @@ struct WindowContainer {
     bool alive = true;
 };
 
-int sdl_init(const char* title, int xpos, int ypos, int width, int height, WindowContainer& wc) {
+int sdl_init(const char* title, int xpos, int ypos, int width, int height, WindowContainer* wc) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         return EXIT_FAILURE;
     }
-    wc.window = SDL_CreateWindow(
+    wc->window = SDL_CreateWindow(
         title,
         xpos,
         ypos,
@@ -24,15 +24,30 @@ int sdl_init(const char* title, int xpos, int ypos, int width, int height, Windo
         height,
         SDL_WINDOW_SHOWN
     );
-    wc.renderer = SDL_CreateRenderer(wc.window, -1, 0);
+    wc->renderer = SDL_CreateRenderer(wc->window, -1, 0);
     return 0;
 }
 
-void sdl_cleanup(WindowContainer& window_container) {
-    SDL_DestroyWindow(window_container.window);
-    SDL_DestroyRenderer(window_container.renderer);
-    window_container.alive = false;
+void sdl_cleanup(WindowContainer* wc) {
+    SDL_DestroyWindow(wc->window);
+    SDL_DestroyRenderer(wc->renderer);
+    wc->alive = false;
     SDL_Quit();
+}
+
+void sdl_default_process_event(SDL_Event* event, WindowContainer* wc) {
+    if (event->type == SDL_QUIT) {
+        sdl_cleanup(wc);
+    }
+}
+
+void sdl_default_render_clear(WindowContainer* wc, SDL_Color clr) {
+    SDL_SetRenderDrawColor(wc->renderer, clr.r, clr.g, clr.b, clr.a);
+    SDL_RenderClear(wc->renderer);
+}
+
+void sdl_default_render_present(WindowContainer* wc) {
+    SDL_RenderPresent(wc->renderer);
 }
 
 #endif // SDL_HJALP_H
